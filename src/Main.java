e202c259535509ea73cc30c1fb649e63d7885fa0
João Vitor Silva dos Santos Oliveira <vitoroliveirasilva438@gmail.com>
2025-03-28 16:40:57 -0300

    testando commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        Cadastrouser pessoa = new Cadastrouser("João", 34, 345.456.543, 0, null, null)
+        Cadastrouser pessoa = new Cadastrouser("João", 34, 345.456.543, 188, "luanpipinelas"nu, null);
+
         
         pessoa.imprimirDados();
         
