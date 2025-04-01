9a5b11075537f5b7be2616dedd855cc985abec62
João Vitor Silva dos Santos Oliveira <vitoroliveirasilva438@gmail.com>
2025-04-01 20:39:25 -0300

    Adicionando Classe da Placa Solar

---
+++ src/Cadastro/PlacaSolar.java
@@ -0,0 +1,83 @@
+package Cadastro;
+
+public class PlacaSolar {
+private String pot;
+private String efi;
+private String temp;
+private String tensao;
+private String corrente;
+private String tpfuncdiario;
+private String status;
+
+public String getCorrente() {
+    return corrente;
+}
+
+public void setCorrente(String corrente) {
+    this.corrente = corrente;
+}
+
+
+public String getEfi() {
+    return efi;
+}
+
+public void setEfi(String efi) {
+    this.efi = efi;
+}
+
+
+public String getPot() {
+    return pot;
+}
+
+public void setPot(String pot) {
+    this.pot = pot;
+}
+
+public String getStatus() {
+    return status;
+}
+
+public void setStatus(String status) {
+    this.status = status;
+}
+
+public String getTemp() {
+    return temp;
+}
+public void setTemp(String temp) {
+    this.temp = temp;
+}
+
+
+public String getTensao() {
+    return tensao;
+}
+
+
+public void setTensao(String tensao) {
+    this.tensao = tensao;
+}
+
+public String getTpfuncdiario() {
+    return tpfuncdiario;
+}
+
+
+public void setTpfuncdiario(String tpfuncdiario) {
+    this.tpfuncdiario = tpfuncdiario;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+}
